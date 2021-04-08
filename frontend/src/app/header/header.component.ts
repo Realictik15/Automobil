874e56bbb306c31b76d1c777285d012aaa603f83
Артем <artem-solovyev@mail.ru>
2021-04-08 22:57:51 +0300

    header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from '../service/token-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLogin = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private tokenStroage: TokenStorageService) {
   }
 
+  ngOnInit(): void {
+    if (this.tokenStroage.getToken()) {
+      this.isLogin = true;
+    }
+  }
+
+  logout(): void {
+    this.tokenStroage.signOut();
+    window.location.reload();
+  }
 }
